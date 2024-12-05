bd3f56061b6a7f03a313729c8af51e36d53e9fa3
Lukas Nordström <luknor@kth.se>
2024-12-05 03:46:47 +0100

    Add mobile navbar
    Added mobile version of the navbar and some basic styling.
    
    Tabler icons was added for easy UI icons.

--- src/components/NavbarView.jsx
+++ src/components/NavbarView.jsx
@@ -1,10 +1,11 @@
 import styles from "../css/Navbar.module.css";
+import { IconMenu2 } from '@tabler/icons-react'
 
 function NavbarView(props) {
 	return (
 		<header id={styles.container}>
-			<nav></nav>
-			<div></div>
+			<h1 className="header-font rounded-corners drop-shadow">BusTrackr</h1>
+			<nav id={styles.headernav} className="rounded-corners drop-shadow"><IconMenu2 height="100%" width="100%" stroke={2}/></nav>
 		</header>
 	);
 }
