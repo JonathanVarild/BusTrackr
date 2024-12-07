d1963549cae7a71d3dd75b43a9ce9ce51917ebd6
JonathanVarild <jonathan.varild@gmail.com>
2024-12-07 22:22:52 +0100

    Added logo along with some minor CSS cleanup

--- src/components/NavbarView.jsx
+++ src/components/NavbarView.jsx
@@ -1,10 +1,13 @@
 import styles from "../css/Navbar.module.css";
 import { IconMenu2 } from '@tabler/icons-react'
+import logo from "../media/logo.png"
 
 function NavbarView(props) {
 	return (
 		<header id={styles.container}>
-			<h1 className="header-font rounded-corners drop-shadow">BusTrackr</h1>
+			<div id={styles.logo} className="rounded-corners drop-shadow">
+				<img src={logo} alt="BusTrackr Logo (BusTrackr LIVE)" />
+			</div>
 			<nav id={styles.headernav} className="rounded-corners drop-shadow"><IconMenu2 height="100%" width="100%" stroke={2}/></nav>
 		</header>
 	);
