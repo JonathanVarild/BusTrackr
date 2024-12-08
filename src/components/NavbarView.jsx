94c90fef5648675f84ad499724355192ba1410b6
Lukas Nordström <luknor@kth.se>
2024-12-08 20:13:01 +0100

    Fix desktop navbar
    Make sure the navbar is correct for the desktop version.
    
    Co-Authored-By: Samuel Brodin <168671080+Pixelshiftor@users.noreply.github.com>

--- src/components/NavbarView.jsx
+++ src/components/NavbarView.jsx
@@ -4,65 +4,60 @@ import logo from "../media/logo.png";
 import ButtonWidget from "./ButtonWidgetView";
 
 function NavbarView(props) {
-  function renderNavigationCB() {
-    return (
-    <>
-      <nav id={styles.navContainer}>
-        <div className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
-          <a href="/">
-            <p>Map</p>
-            <div className={styles.iconContainer}>
-              <IconMap stroke={2.5} />
-            </div>
-          </a>
-        </div>
-        <div className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
-          <a href="/about">
-            <p>About</p>
-            <div className={styles.iconContainer}>
-              <IconQuestionMark stroke={2.5} />
-            </div>
-          </a>
-        </div>
-        <div className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
-          <a href="/account">
-            <p>Account</p>
-            <div className={styles.iconContainer}>
-              <IconUser stroke={2.5} />
-            </div>
-          </a>
-        </div>
-        <div className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
-          <a href="/attribution">
-            <p>Attribution</p>
-            <div className={styles.iconContainer}>
-              <IconUsers stroke={2.5} />
-            </div>
-          </a>
-        </div>
-      </nav>
-      <div className={`${styles.closeContainer}`}>
-        <ButtonWidget
-          iconElement={<IconX stroke={2} />}
-          onClick={props.onToggleNav}
-        />
-      </div>
-    </>
-    );
-  }
-
   return (
     <header id={styles.container} className={`${props.isNavOpen ? styles.navOpen : ""}`}>
       <div id={styles.logo} className='rounded-corners drop-shadow blurred-background'>
         <img src={logo} alt='BusTrackr Logo (BusTrackr LIVE)' />
       </div>
       <div id={styles.headernav}>
-        {props.isNavOpen ? renderNavigationCB() : <ButtonWidget
-          iconElement={<IconMenu2 stroke={2} />}
-          onClick={props.onToggleNav}
-        />}
+        <nav className={props.isNavOpen ? styles.navContainerOpen : styles.navContainerClosed}>
+          <div id={styles.mapLink} className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
+            <a href="/">
+              <p>Map</p>
+              <div className={styles.iconContainer}>
+                <IconMap stroke={2.5} />
+              </div>
+            </a>
+          </div>
+          <div id={styles.aboutLink} className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
+            <a href="/about">
+              <p>About</p>
+              <div className={styles.iconContainer}>
+                <IconQuestionMark stroke={2.5} />
+              </div>
+            </a>
+          </div>
+          <div id={styles.accountLink} className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
+            <a href="/account">
+              <p>Account</p>
+              <div className={styles.iconContainer}>
+                <IconUser stroke={2.5} />
+              </div>
+            </a>
+          </div>
+          <div id={styles.attributionLink} className={`${styles.navItem} header-font rounded-corners drop-shadow blurred-background`}>
+            <a href="/attribution">
+              <p>Attribution</p>
+              <div className={styles.iconContainer}>
+                <IconUsers stroke={2.5} />
+              </div>
+            </a>
+          </div>
+        </nav>
+        <div className={`${styles.closeContainer}`}>
+          {props.isNavOpen ?
+            <ButtonWidget
+              iconElement={<IconX stroke={2} />}
+              onClick={props.onToggleNav}
+            />
+            :
+            <ButtonWidget
+              iconElement={<IconMenu2 stroke={2} />}
+              onClick={props.onToggleNav}
+            />
+          }
+        </div>
       </div>
-			
     </header>
   );
 }
