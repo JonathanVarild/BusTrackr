3745fc24910dd0036456a2feca68e2719cc314ed
Lukas Nordström <luknor@kth.se>
2024-12-08 18:05:24 +0100

    Fix math
    Make it not be so big on desktops.
    
    Co-Authored-By: Samuel Brodin <168671080+Pixelshiftor@users.noreply.github.com>

--- src/components/MapSearchBar.jsx
+++ src/components/MapSearchBar.jsx
@@ -4,13 +4,15 @@ import ButtonWidget from "./ButtonWidgetView";
 
 function SearchBar(props) {
   return (
-    <div id={styles.container}>
-      <input type="text" placeholder="Search..." className={`${styles.input} header-2-font blurred-background rounded-corners drop-shadow`}/>
-      <ButtonWidget
-        id={styles.button}
-        onClick={onSearchACB}
-        iconElement={<IconSearch stroke={2} />}
-      />
+    <div id={styles.outerContainer} className={`${styles.justifyContent}`}>
+      <div id={styles.container}>
+        <input type="text" placeholder="Search..." className={`${styles.input} header-2-font blurred-background rounded-corners drop-shadow`}/>
+        <ButtonWidget
+          id={styles.button}
+          onClick={onSearchACB}
+          iconElement={<IconSearch stroke={2} />}
+        />
+      </div>
     </div>
   );
 }
