8faf02a92b5e87f618cb9a18a652ae7e8b4ebe94
JonathanVarild <jonathan.varild@gmail.com>
2024-12-09 01:02:22 +0100

    Made ButtonWidget allow custom classes

--- src/components/ButtonWidgetView.jsx
+++ src/components/ButtonWidgetView.jsx
@@ -1,22 +1,20 @@
-import styles from "../css/ButtonWidget.module.css"
+import styles from "../css/ButtonWidget.module.css";
 
 function ButtonWidget(props) {
-  function onClickACB(event) {
-    if (props.onClick) {
-      props.onClick(event);
-    }
-  };
+	function onClickACB(event) {
+		if (props.onClick) {
+			props.onClick(event);
+		}
+	}
 
-  return (
-    <button
-      id={props.id ? props.id : undefined}
-      className={`${styles.uiWidget} blurred-background rounded-corners drop-shadow`}
-      onClick={onClickACB}>
-      <div className={`${styles.uiIconContainer}`}>
-        {props.iconElement}
-      </div>
-    </button>
-  );
+	let classNames = props?.className?.split(" ") || [];
+	classNames = [...classNames, styles.uiWidget, "blurred-background", "rounded-corners", "drop-shadow"];
+
+	return (
+		<button id={props.id} className={classNames.join(" ")} onClick={onClickACB}>
+			<div className={`${styles.uiIconContainer}`}>{props.iconElement}</div>
+		</button>
+	);
 }
 
 export default ButtonWidget;
