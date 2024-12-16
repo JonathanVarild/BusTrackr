6340bfcb02a54cbc19b4f28b4bbf8b9ccb7f6da2
JonathanVarild <jonathan.varild@gmail.com>
2024-12-16 21:01:14 +0100

    Added a temporary fix for compatability issue between Vite and Tabler icons

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
-import reactJsxPlugin from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import reactJsxPlugin from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [
-        reactJsxPlugin(),
-    ],
-    server: {
-        port: 8080,
-    },
-    build: {
-        sourcemap: true,
-        minify: false
-    }
+	plugins: [reactJsxPlugin()],
+	server: {
+		port: 8080,
+	},
+	build: {
+		sourcemap: true,
+		minify: false,
+	},
+	resolve: {
+		alias: {
+			// Force Vite to resolve only static exports
+			// See issue: https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+			"@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+		},
+	},
 });
