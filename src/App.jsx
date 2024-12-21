1bc6546d70e0bd1ef38a092d08f5bbc58f1191f5
JonathanVarild <jonathan.varild@gmail.com>
2024-12-21 05:09:12 +0100

    Added page spinner until re-auth has finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { reauthenticateUser } from "./store/interface/authentication";
 
 import Navbar from "./presenters/NavbarPresenter";
@@ -12,15 +12,19 @@ import AccountSettingsPresenter from "./presenters/AccountSettingsPresenter";
 import PopupBox from "./presenters/PopupBoxPresenter";
 import AuthPopupPresenter from "./presenters/AuthPopupPresenter";
 import DataReport from "./presenters/DataReportPresenter";
+import LoadingSpinnerView from "./views/LoadingSpinnerView";
 
 import "./css/global.css";
 
 function App() {
+	const reauthStatus = useSelector((state) => state.interface.reauthenticate.status);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(reauthenticateUser());
 	}, [dispatch]);
+
+	console.log(reauthStatus);
 
 	return (
 		<>
@@ -36,6 +40,7 @@ function App() {
 			<AccountSettingsPresenter />
 			<AuthPopupPresenter />
 			<PopupBox />
+			{reauthStatus === "loading" && <LoadingSpinnerView />}
 		</>
 	);
 }
