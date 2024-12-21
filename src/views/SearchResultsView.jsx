eb7e573af54ef6a3314f5e76c393c7301a0bce92
Lukas Nordström <luknor@kth.se>
2024-12-22 00:18:44 +0100

    Minor formatting

--- src/views/SearchResultsView.jsx
+++ src/views/SearchResultsView.jsx
@@ -31,17 +31,16 @@ function SearchResultsView(props) {
         }
     }
 
-
     function titleCB() {
         return (
             <div id={styles.resultTitle}>
                 <div className={styles.pageSwitcherContainer}>
                     <button className={styles.pageSwitcherBtn} onClick={getPrevPageACB} disabled={props.searchResults?.page <= 0}>
-                        <IconCaretLeft className={styles.pageSwitcherIcon} stroke={1.25}/>
+                        <IconCaretLeft className={styles.pageSwitcherIcon} stroke={1.25} />
                     </button>
                 </div>
                 <div className={styles.pageVisualizer}>
-                    <p>{(props.searchResults.page + 1) + " / " + Math.ceil(props.searchResults.total / 5)}</p>
+                    <p>{props.searchResults.page + 1 + " / " + Math.ceil(props.searchResults.total / 5)}</p>
                 </div>
                 <div className={styles.pageSwitcherContainer}>
                     <button className={styles.pageSwitcherBtn} onClick={getNextPageACB} disabled={props.searchResults?.is_last_page}>
@@ -49,40 +48,26 @@ function SearchResultsView(props) {
                     </button>
                 </div>
             </div>
-        )
-        
+        );
     }
 
     function renderBusResult(result, index) {
         return (
-            <div
-            key={index}
-            className={styles.resultRow}>
+            <div key={index} className={styles.resultRow}>
                 <h3 className={`${styles.busDisplay} ${getBusColorStyle(result.line)}`}>
                     <span className={styles.lineDisp}>{result.line}</span>
                     <span className={styles.displaySim}>{result.destination}</span>
                 </h3>
                 <p>stops at {result.stop_name}</p>
             </div>
-        )
+        );
     }
 
     function contentCB() {
-        return (
-            <div id={styles.resultsContainer}>
-                {props.searchResults?.results?.map(renderBusResult)}
-            </div>
-        )
+        return <div id={styles.resultsContainer}>{props.searchResults?.results?.map(renderBusResult)}</div>;
     }
 
-    return (
-        <BoxWidgetView
-            onCloseClick={onCloseClickACB}
-            dataStatus={props.searchStatus}
-            renderTitleElementCB={titleCB}
-            renderContentElementCB={contentCB}
-        />
-    );
+    return <BoxWidgetView onCloseClick={onCloseClickACB} dataStatus={props.searchStatus} renderTitleElementCB={titleCB} renderContentElementCB={contentCB} />;
 }
 
 export default SearchResultsView;
