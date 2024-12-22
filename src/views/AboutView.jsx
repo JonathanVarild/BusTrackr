21f5c771a7d865f00336d1b51981959b2221b8ab
JonathanVarild <jonathan.varild@gmail.com>
2024-12-22 21:59:46 +0100

    Changes to About page

--- src/views/AboutView.jsx
+++ src/views/AboutView.jsx
@@ -4,19 +4,32 @@ function AboutView() {
 	return (
 		<section>
 			<div id={styles.body}>
-				<div id={styles.inner} className="drop-shadow rounded-corners">
+				<div id={styles.inner}>
 					<h1>About The Project</h1>
-					<p>
+					<p className={styles.bottomMargin}>
 						BusTrackr is app created by Jonathan Värild, Lukas Nordström and Samuel Brodin as the project part of the course DH2642 Interaction Programming and the
-						Dynamic Web (iprogdh) at KTH Royal Institute of Technology in Sweden. The application will allow users to track Stockholm local transport buses on a live
-						map, create their own account, track favorite buses, etc.
+						Dynamic Web (iprogdh) at KTH Royal Institute of Technology in Sweden. The application allows users to track Stockholm local transport buses on a live map,
+						create their own account, track favorite buses, etc.
+					</p>
+					<p>
+						Check out the{" "}
+						<a href="https://github.com/JonathanVarild/BusTrackr" target="_blank">
+							GitHub Repository
+						</a>{" "}
+						for more details.
+					</p>
+					<p>
+						Check out the backend{" "}
+						<a href="https://github.com/JonathanVarild/BusTrackr" target="_blank">
+							here.
+						</a>{" "}
 					</p>
 					<h2>The Team</h2>
 					<ul>
 						<li>
 							<strong>Jonathan Värild</strong>
 							<ul>
-								<li>Computer Engineering student of 2023</li>
+								<li>Second-year student in the Degree Programme in Computer Engineering (TIDAB)</li>
 								<li>
 									<a href="https://www.linkedin.com/in/jonathanvärild/">LinkedIn</a>
 								</li>
@@ -25,26 +38,19 @@ function AboutView() {
 						<li>
 							<strong>Lukas Nordström</strong>
 							<ul>
-								<li>Computer Engineering student of 2023</li>
+								<li>Second-year student in the Degree Programme in Computer Engineering (TIDAB)</li>
 								<li>
 									<a href="https://www.linkedin.com/in/lukas-nordström-27959528a/">LinkedIn</a>
 								</li>
 							</ul>
 						</li>
 						<li>
-							<strong>Samuel Brodin (Computer Engineering student of 2023)</strong>
+							<strong>Samuel Brodin</strong>
 							<ul>
-								<li>Computer Engineering student of 2023</li>
+								<li>Second-year student in the Degree Programme in Computer Engineering (TIDAB)</li>
 							</ul>
 						</li>
 					</ul>
-					<p>
-						Check out the{" "}
-						<a href="https://github.com/JonathanVarild/BusTrackr" target="_blank">
-							GitHub Repository
-						</a>{" "}
-						for more details.
-					</p>
 				</div>
 			</div>
 		</section>
