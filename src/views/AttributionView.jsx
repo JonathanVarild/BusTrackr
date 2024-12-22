c488a256f85bed62494b9874f3f064533ff7093e
Samuel Brodin <168671080+Pixelshiftor@users.noreply.github.com>
2024-12-22 15:53:32 +0100

    Attribution View Draft

--- src/views/AttributionView.jsx
+++ src/views/AttributionView.jsx
@@ -1,5 +1,45 @@
-function AttributionView() {
-	return <section>Section tag adds top margin</section>;
-}
+import styles from "../css/About.module.css";
 
-export default AttributionView;
+function AttributionView() {
+	return (
+	  <section>
+		<div id={styles.body}>
+			<div id={styles.inner} className="drop-shadow rounded-corners">
+			<h1>Attribution</h1>
+			<p>
+				This project was made possible thanks to the contributions of the following tools, libraries, and individuals.
+			</p>
+			<h2>Tools and Libraries</h2>
+			<ul>
+				<li>
+				<strong>React:</strong> A JavaScript library for building user interfaces. <a href="https://reactjs.org" target="_blank">Learn more</a>
+				</li>
+				<li>
+				<strong>Redux:</strong> A library for simple, scalable state management. <a href="https://redux.js.org/" target="_blank">Learn more</a>
+				</li>
+				{/* More */}
+			</ul>
+			<h2>Images and Icons</h2>
+			<ul>
+				<li>
+				Icons provided by <a href="https://tabler.io/icons" target="_blank">Tabler icons</a>
+				</li>
+				{/* More */}
+			</ul>
+			<h2>People {/* Maybe for the tester we got feedback from? Or our coach? */} </h2> 
+			<ul>
+				<li>
+				<strong>Name:</strong> [...]
+				</li>
+				<li>
+				<strong>Name:</strong> [...]
+				</li>
+			</ul>
+			</div>
+		</div>
+	  </section>
+	);
+  }
+  
+  export default AttributionView;
+  
